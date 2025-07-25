583f9ed963af855e101af13ae3d5a20d20bcfb1f
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-07-25 14:19:03 -0700

    add forward geocode to Geocoder interface

--- src/app/grapheneos/geocoder/Geocoder.kt
+++ src/app/grapheneos/geocoder/Geocoder.kt
@@ -4,6 +4,16 @@ import android.location.Address
 import java.util.Locale
 
 interface Geocoder {
+    fun forwardGeocode(
+        locationName: String,
+        lowerLeftLatitude: Double,
+        lowerLeftLongitude: Double,
+        upperRightLatitude: Double,
+        upperRightLongitude: Double,
+        maxResults: Int,
+        preferredLocale: Locale,
+    ): List<Address>
+
     fun reverseGeocode(
         latitude: Double,
         longitude: Double,
